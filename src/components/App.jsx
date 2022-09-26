ed4563c97b90144d3682875c20958bf19c490ccd
mikegurak <mykhailohurak@gmail.com>
2022-09-26 20:56:24 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export default class App extends Component {
+
+  state = {
+    pokemon: null,
+    loading: false
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+  
+    fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+      .then(result => result.json())
+      .then((pokemon) => (this.setState({ pokemon })))
+      .finally(() => (this.setState({ loading: false })))
+  }
+
+  render() {
+    return (
+      // <div>APP</div>
+      <div>
+        {this.state.loading && <h1>Loading...</h1>}
+        {this.state.pokemon && <div>{this.state.pokemon.name}</div>}
+      </div>
+    )
+  }
+}
